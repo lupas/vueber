8669a83955868da31158f1401a25d60782093e2a
Pascal <pascal@luther.ch>
2019-11-21 11:19:50 +0800

    removed mapGetters

--- packages/vueber-fire/src/components/index.vue
+++ packages/vueber-fire/src/components/index.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
+import { mapState, mapActions, mapMutations } from 'vuex'
 import { unwrapFirestoreDoc } from 'firewings'
 
 export default {

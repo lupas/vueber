b66f14c0d8fb53fa7acf0b3213240c2ef3f488e7
Pascal <pascal@luther.ch>
2019-11-21 14:57:56 +0800

    renamev unreadConversations

--- packages/vueber-fire/src/index.js
+++ packages/vueber-fire/src/index.js
@@ -1,8 +1,8 @@
 import VueberFire from './components/main.vue'
-import VfUnreadConversations from './components/unreadConversations.vue'
+import unreadConversations from './components/unreadConversations.vue'
 
 // Register Components
 
-export { VueberFire, VfUnreadConversations }
+export { VueberFire, unreadConversations }
 
 export const meta = require('../package.json')
